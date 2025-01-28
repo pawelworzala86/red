3d815db4dd57271f7cc63534ee9688a46881c052
Pawel Worzala <pawelworzala86@gmail.com>
2025-01-28 11:55:39 +0100

    auto save

--- source/directives/show.js
+++ source/directives/show.js
@@ -2,17 +2,17 @@
 
     const Directives=node('Directives')
     
-    function iff(element, scope, attr){
+    function show(element, scope, attr){
         const change = (val) => {
             let rif = (function (attribute) {
                 return eval(attribute) ? true : false;
             }.bind(scope))(attr);
             console.log('rif...', rif)
             if (rif) {
-                //element.classList.remove("hide");
+                element.classList.remove("hide");
             } else {
-                //element.classList.add("hide");
-                element.remove()
+                element.classList.add("hide");
+                //element.remove()
             }
         };
         const regex = new RegExp("this.([a-zA-Z0-9]+)");
@@ -23,7 +23,7 @@
         change();
     }
     
-    Directives["if"]=iff
+    Directives["show"]=show
     
 
 }
