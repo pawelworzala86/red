e67d152a0aa61657802de340f6a80b005635d0b2
Pawel Worzala <pawelworzala86@gmail.com>
2025-01-28 13:09:25 +0100

    auto save

--- article.js
+++ article.js
@@ -5,11 +5,41 @@ const texts = [
         content: [
             {
                 type:'text',
-                text: 'Jestem doświadczonym programistą specjalizującym się w PHP i JavaScript, z ponad 5-letnim doświadczeniem w tworzeniu złożonych aplikacji i skryptów. Moja wiedza obejmuje głębokie zrozumienie programowania obiektowego, co pozwala mi na projektowanie i implementację skalowalnych oraz wydajnych rozwiązań. Posiadam zaawansowane umiejętności w zakresie projektowania i zarządzania bazami danych, co umożliwia efektywne przechowywanie, przetwarzanie i analizę danych.'
+                text: `Podaję kod do DI w JavaScript. Za pomocą tego kodu możemy wstrzykiwać do parametrów funkcji zasoby z 'fabryki’. W podanym przykładzie wstrzykujemy do funkcji testFunc zasoby które odpowiadaja nazwom z wartości FACTORY. Rozwiązanie się przydaje np. kiedy mamy routes napisane w JS i ładujemy template pod url’e i mamy callback do wypełniania np. scope’ów – a parmetry callbacka to sa np. nie param1 jak w przykładzie tylko np. http, rootScope, scope i inne fabrykowane obiekty. Ogolnie DI stosuję kiedy mam callbacki które sa wywoływane bez wcześniejszego ustalania parametrów funkcji a ich dane wejsciowe są zawsze różne i nie zawsze w tej samej kolejnosci – po prostu automat wstrzykuje te parametry i po to jest fabryka obiektów.`
             },
             {
-                type:'text',
-                text: 'Aktywnie korzystam z nowoczesnych narzędzi AI, które wspomagają mnie w codziennej pracy programistycznej. Wykorzystuję AI do optymalizacji fragmentów kodu, przetwarzania danych oraz konwersji tekstu na tabelki i odwrotnie. Ponadto, AI wspiera mnie w realizacji złożonych zadań w aplikacjach, co znacząco podnosi efektywność mojej pracy i jakość dostarczanych rozwiązań'
+                type:'code',
+                text: `const FACTORY = {
+    'param1': function(){
+        return 'test parametru'
+    },
+}
+
+function DI(callback){
+    const text = callback.toString()
+    let params = /\(.*\)/.exec(text)[0]
+    params = params.substring(1,params.length-1)
+    params = params.split(',')
+    params = params.map(param=>{
+        let fromFactory = FACTORY[param]
+        if(typeof fromFactory=='function'){
+            fromFactory = fromFactory()
+        }else{
+            fromFactory = param
+        }
+        return fromFactory
+    })
+    return callback(...params)
+}
+
+
+
+
+const testFunc = function(param1,param2){
+    console.log('param1 = ',param1)
+}
+
+DI(FACTORY,testFunc)`
             }
         ]
     },
--- tools/di.js
+++ tools/di.js
@@ -1,23 +1,14 @@
-const testFunc = function(param1,param2){
-    console.log('param1 = ',param1)
-}
-
-
 const FACTORY = {
     'param1': function(){
         return 'test parametru'
     },
 }
 
-
 function DI(callback){
     const text = callback.toString()
     let params = /\(.*\)/.exec(text)[0]
-    //console.log(params)
-    //params = params[0]
     params = params.substring(1,params.length-1)
     params = params.split(',')
-    //console.log(params)
     params = params.map(param=>{
         let fromFactory = FACTORY[param]
         if(typeof fromFactory=='function'){
@@ -30,7 +21,11 @@ function DI(callback){
     return callback(...params)
 }
 
-DI(testFunc)
 
 
-//console.log('end')
+
+const testFunc = function(param1,param2){
+    console.log('param1 = ',param1)
+}
+
+DI(FACTORY,testFunc)
